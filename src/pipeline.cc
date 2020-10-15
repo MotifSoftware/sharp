183bfbc9301d5188f7118c12f04905d47fd314d9
Stephen Mann <steve@motifsoftware.com>
2020-10-15 16:02:30 -0400

    Removed all icc transforms.

--- src/pipeline.cc
+++ src/pipeline.cc
@@ -376,10 +376,10 @@ public:
         // Convert to sRGB using embedded profile
         try
         {
-          image = image.icc_transform("srgb", VImage::option()
+          /*image = image.icc_transform("srgb", VImage::option()
                                                   ->set("embedded", TRUE)
                                                   ->set("depth", image.interpretation() == VIPS_INTERPRETATION_RGB16 ? 16 : 8)
-                                                  ->set("intent", VIPS_INTENT_PERCEPTUAL));
+                                                  ->set("intent", VIPS_INTENT_PERCEPTUAL));*/
         }
         catch (...)
         {
@@ -388,9 +388,9 @@ public:
       }
       else if (image.interpretation() == VIPS_INTERPRETATION_CMYK)
       {
-        image = image.icc_transform("srgb", VImage::option()
+        /*image = image.icc_transform("srgb", VImage::option()
                                                 ->set("input_profile", "srgb")
-                                                ->set("intent", VIPS_INTENT_PERCEPTUAL));
+                                                ->set("intent", VIPS_INTENT_PERCEPTUAL));*/
       }
 
       // Flatten image to remove alpha channel
@@ -819,6 +819,26 @@ public:
       {
         // Convert colourspace, pass the current known interpretation so libvips doesn't have to guess
         image = image.colourspace(baton->colourspace, VImage::option()->set("source_space", image.interpretation()));
+      }
+
+      // Set ICC profile
+      if (!baton->withMetadataIcc.empty())
+      {
+        VipsBlob *blob = NULL;
+        if (vips_profile_load(const_cast<char *>(baton->withMetadataIcc.data()), &blob, NULL) == 0)
+        {
+          vips_image_set_blob(
+              image.get_image(),
+              "icc-profile-data",
+              blob->area.free_fn,
+              blob->area.data,
+              blob->area.length);
+        }
+
+        /*image = image.icc_import(
+            VImage::option()
+                ->set("input_profile", const_cast<char *>(baton->withMetadataIcc.data()))
+                ->set("intent", VIPS_INTENT_PERCEPTUAL));*/
       }
 
       // Override EXIF Orientation tag
